3e45c407983fcef28cdee2581100ba445e394ca4
mehak2006 <69978573+mehak2006@users.noreply.github.com>
2026-05-07 01:53:04 +0530

    Fixed k parsing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.responses import JSONResponse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import Response
 from fastapi import HTTPException
 from kmeans import compress_image_kmeans
@@ -31,7 +31,10 @@ async def root():
 
 
 @app.post('/compress')
-async def compress_image(file: UploadFile = File(...), k: int = 16):
+async def compress_image(
+    file: UploadFile = File(...),
+    k: int = Form(16)
+):
     if k <= 1 or k > 64:
         raise HTTPException(status_code=400, detail="K must be between 2 and 64")
     contents = await file.read()
